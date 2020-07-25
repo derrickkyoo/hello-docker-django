9098d1d17d6739efc3202a698d704ec26549f86f
Derrick Yoo <4998684+derrickyoo@users.noreply.github.com>
2020-07-24 22:17:55 -0700

    Setup staticfiles

--- hello/settings.py
+++ hello/settings.py
@@ -126,6 +126,16 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "/code/hello/static")]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
 
 # Custom user
+
 AUTH_USER_MODEL = "users.User"
