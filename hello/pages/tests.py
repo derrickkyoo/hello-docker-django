a7ddd7005b89fe771e0ee931093d0fad4c4fd3b2
Derrick Yoo <4998684+derrickyoo@users.noreply.github.com>
2020-09-09 21:31:21 -0700

    Add test for home page

--- hello/pages/tests.py
+++ hello/pages/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
 
-# Create your tests here.
+from .views import HomePageView
+
+
+class HomepageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse("pages:home")
+        self.response = self.client.get(url)
+
+    def test_homepage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, "pages/home.html")
+
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.response, "Home")
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, "About")
+
+    def test_hompage_url_resolves_homepageview(self):
+        view = resolve("/")
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
